5e7402927214ed9bfdd362d2924da0d2cdea7820
tengex <tengex@inf.elte.hu>
2016-11-27 23:49:14 +0100

    Teljes szerveroldali funkcionalitás

--- app/Http/Controllers/IndexController.js
+++ app/Http/Controllers/IndexController.js
@@ -6,59 +6,96 @@ const Trip = use('App/Model/Trip')
 
 class IndexController {
     * showIndexPage(req, res) {
-        //try {
-        var success = false;
-        var whereCriteria = {};
-        if (req.currentUser) {
-            if (req.currentUser.username == 'Admin') {
-                whereCriteria = { 'end_date': null };
-            } else if (req.currentUser.is_active) {
-                whereCriteria = { 'employee': req.currentUser.username, 'end_date': null }
-            }
+        try {
+            var success = false;
+            if (req.currentUser) {
 
-            const entities = yield Database.select('*').from('trips').where(whereCriteria);
+                //Unclosed Trips***************************************************
+                var whereCriteria = {};
+                if (req.currentUser.username == 'Admin') {
+                    whereCriteria = { 'end_date': null };
+                } else if (req.currentUser.is_active) {
+                    whereCriteria = { 'employee': req.currentUser.username, 'end_date': null }
+                }
 
-            for (var i in entities) {
-                var date = new Date(entities[i].start_date * 1000);
-                var year = date.getFullYear();
-                var month = "0" + date.getMonth();
-                var day = "0" + date.getDate();
-                var hours = date.getHours();
-                var minutes = "0" + date.getMinutes();
-                var seconds = "0" + date.getSeconds();
-                entities[i].start_date = year + "." + month.substr(-2) + "." + day.substr(-2) + ". " + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+                const unclosedTrips = yield Database.select('*').from('trips').where(whereCriteria);
 
-                if (entities[i].end_date != null) {
-                    date = new Date(entities[i].end_date * 1000);
-                    year = date.getFullYear();
-                    month = "0" + date.getMonth();
-                    day = "0" + date.getDate();
-                    hours = date.getHours();
-                    minutes = "0" + date.getMinutes();
-                    seconds = "0" + date.getSeconds();
-                    entities[i].end_date = year + "." + month.substr(-2) + "." + day.substr(-2) + ". " + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+                for (var i in unclosedTrips) {
+                    var date = new Date(unclosedTrips[i].start_date * 1000);
+                    unclosedTrips[i].start_date = timestampToDate(date);
+
+                    if (unclosedTrips[i].end_date != null) {
+                        date = new Date(unclosedTrips[i].end_date * 1000);
+                        unclosedTrips[i].end_date = timestampToDate(date);
+                    }
+                }
+
+                //Vehicles Trips***************************************************
+                var vehiclesTrips = [];
+                const vehiclesList = yield Database.select('license_plate').from('vehicles');
+
+                for (var i in vehiclesList) {
+                    const tripList = yield Database.select(['id', 'from_site', 'to_site', 'shipment', 'start_date', 'end_date']).from('trips').where('vehicle', vehiclesList[i].license_plate);
+
+                    for (var j in tripList) {
+                        var date = new Date(tripList[j].start_date * 1000);
+                        tripList[j].start_date = timestampToDate(date);
+
+                        if (tripList[j].end_date != null) {
+                            date = new Date(tripList[j].end_date * 1000);
+                            tripList[j].end_date = timestampToDate(date);
+                        }
+                    }
+
+                    vehiclesTrips[vehiclesTrips.length] = { license_plate: vehiclesList[i].license_plate, tripList };
+                }
+
+                //Employees Vehicles***********************************************
+                var employeesVehicles = [];
+                const employeesList = yield Database.select(['username', 'fullname']).from('employees');
+
+                for (var i in employeesList) {
+                    const tripList = yield Database.select('vehicle').from('trips').where('employee', employeesList[i].username).distinct();
+                    var vehicleList = [];
+
+                    for (var j in tripList) {
+                        const vehicleInfo = yield Database.select('*').from('vehicles').where('license_plate', tripList[j].vehicle);
+                        vehicleList[vehicleList.length] = vehicleInfo[0];
+                    }
+
+                    employeesVehicles[employeesVehicles.length] = { username: employeesList[i].username, fullname: employeesList[i].fullname, vehicleList }
+                }
+
+                yield res.sendView('index', {
+                    unclosedTrips,
+                    vehiclesTrips,
+                    employeesVehicles
+                });
+                success = true;
+
+                if (!success) {
+                    yield res.sendView('errors.permissionError');
                 }
             }
-            console.log(entities)
-            yield res.sendView('index', {
-                entities,
-            });
-            success = true;
-
-
-            if (!success) {
-                yield res.sendView('errors.permissionError');
+            else {
+                yield res.sendView('index');
             }
-        }
-        else {
-            yield res.sendView('index');
-        }
 
-        /*}
+        }
         catch (e) {
             yield res.sendView('errors.unexpectedError');
-        }*/
+        }
     }
+}
+
+function timestampToDate(date) {
+    var year = date.getFullYear();
+    var month = "0" + date.getMonth();
+    var day = "0" + date.getDate();
+    var hours = date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var seconds = "0" + date.getSeconds();
+    return (year + "." + month.substr(-2) + "." + day.substr(-2) + ". " + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2));
 }
 
 module.exports = IndexController
